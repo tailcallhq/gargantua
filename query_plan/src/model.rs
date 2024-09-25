cfbf9b89056e25ff8d19c96e3510d240172c3f06
Tushar Mathur <tusharmath@gmail.com>
2024-09-25 21:01:31 +0530

    update model with pub fields

--- query_plan/src/model.rs
+++ query_plan/src/model.rs
@@ -6,40 +6,40 @@ pub enum QueryPlan<Value> {
     Parallel(Vec<QueryPlan<Value>>),
     Sequence(Vec<QueryPlan<Value>>),
     Fetch {
-        service: GraphId,
-        query: SelectionSet<Value>,
-        representations: Option<SelectionSet<Value>>,
-        type_name: String,
+        pub service: GraphId,
+        pub query: SelectionSet<Value>,
+        pub representations: Option<SelectionSet<Value>>,
+        pub type_name: String,
     },
     Flatten {
-        path: Lens,
-        plan: Box<QueryPlan<Value>>,
+        pub path: Lens,
+        pub plan: Box<QueryPlan<Value>>,
     },
 }
 
 #[derive(Debug, Clone, Setters)]
 pub struct SelectionSet<Value> {
-    fields: Vec<Field<Value>>,
+    pub fields: Vec<Field<Value>>,
 }
 
 #[derive(Debug, Clone, Setters)]
 pub struct Field<Value> {
-    name: String,
-    selections: SelectionSet<Value>,
-    arguments: Vec<Argument<Value>>,
-    directives: Vec<Directive<Value>>,
+    pub name: String,
+    pub selections: SelectionSet<Value>,
+    pub arguments: Vec<Argument<Value>>,
+    pub directives: Vec<Directive<Value>>,
 }
 
 #[derive(Debug, Clone, Setters)]
 pub struct Argument<Value> {
-    name: String,
-    value: Value,
+    pub name: String,
+    pub value: Value,
 }
 
 #[derive(Debug, Clone, Setters)]
 pub struct Directive<Value> {
-    name: String,
-    arguments: Vec<Argument<Value>>,
+    pub name: String,
+    pub arguments: Vec<Argument<Value>>,
 }
 
 #[derive(Debug, Clone)]

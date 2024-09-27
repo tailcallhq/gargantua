a875e68baac3dc59d69032bb795f5fbb15a7f69b
Tushar Mathur <tusharmath@gmail.com>
2024-09-27 18:11:33 +0530

    refactor: move test to build

--- blueprint/src/build.rs
+++ blueprint/src/build.rs
@@ -528,3 +528,21 @@ fn parse_argument(
         )),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use valid::Validator;
+
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let graphql = resource::resource_str!("../examples/router.graphql");
+        let document = async_graphql_parser::parse_schema(graphql).unwrap();
+        let blueprint = parse(document)
+            .map(|b| serde_json::to_string_pretty(&b).unwrap())
+            .to_result()
+            .unwrap();
+        insta::assert_snapshot!(blueprint);
+    }
+}
--- blueprint/src/lib.rs
+++ blueprint/src/lib.rs
@@ -1,25 +1,8 @@
 mod blueprint;
 mod build;
-pub mod error;
 mod index;
+
+// Public API
+pub mod error;
 pub use blueprint::*;
-pub use build::parse;
-pub use {async_graphql_parser, async_graphql_value, url};
-
-#[cfg(test)]
-mod tests {
-    use valid::Validator;
-
-    use super::*;
-
-    #[test]
-    fn test_parse() {
-        let graphql = resource::resource_str!("../examples/router.graphql");
-        let document = async_graphql_parser::parse_schema(graphql).unwrap();
-        let blueprint = parse(document)
-            .map(|b| serde_json::to_string_pretty(&b).unwrap())
-            .to_result()
-            .unwrap();
-        insta::assert_snapshot!(blueprint);
-    }
-}
+pub use index::*;

a64d0dac9c3bc2ccdfdaba7d742091c987be1136
laststylebender <ran.mahadik14@gmail.com>
2024-09-29 14:28:56 +0530

    - moved conversion of async selection set to selection set in  model.

--- query_plan/src/build.rs
+++ query_plan/src/build.rs
@@ -1,5 +1,4 @@
-use crate::{Field, SelectionSet};
-use async_graphql::Positioned;
+use crate::SelectionSet;
 use async_graphql_parser::types::{self as Q};
 
 pub struct Builder<A> {
@@ -13,48 +12,5 @@ impl<A: Default> Builder<A> {
 
     pub fn build(&self, operation: &Q::OperationDefinition) -> SelectionSet<A> {
         SelectionSet::from(&operation.selection_set.node)
-    }
-}
-
-impl<A: Default> From<&Q::SelectionSet> for SelectionSet<A> {
-    fn from(node: &Q::SelectionSet) -> SelectionSet<A> {
-        let mut selection_set: SelectionSet<A> = SelectionSet::default();
-        for selection in node.items.iter() {
-            let inner_selection = &selection.node;
-            match inner_selection {
-                Q::Selection::Field(Positioned { node, .. }) => {
-                    let field_name = node.name.node.as_str().to_string();
-                    let field =
-                        Field::new(field_name, SelectionSet::from(&node.selection_set.node));
-                    selection_set.push(field);
-                }
-                Q::Selection::InlineFragment(Positioned { node, .. }) => {
-                    todo!()
-                }
-                Q::Selection::FragmentSpread(Positioned { node, .. }) => {
-                    todo!()
-                }
-            }
-        }
-        selection_set
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use crate::{Builder, SelectionSet};
-    use insta::assert_debug_snapshot;
-
-    #[test]
-    fn test() {
-        // Query
-        let query = "query { topProducts { name reviews { score } reviews { description } } }";
-        let p_query = async_graphql_parser::parse_query(query).unwrap();
-        let op = match p_query.operations {
-            async_graphql_parser::types::DocumentOperations::Single(op) => op.node,
-            _ => todo!(),
-        };
-        let selection_set: SelectionSet<String> = Builder::new().build(&op);
-        assert_debug_snapshot!(selection_set);
     }
 }
--- query_plan/src/model.rs
+++ query_plan/src/model.rs
@@ -1,9 +1,9 @@
 use std::rc::Rc;
 
-use async_graphql_parser::types::ExecutableDocument;
-use blueprint::{Graph, Index, JoinField, JoinGraph};
+use async_graphql::Positioned;
+use async_graphql_parser::types::{self as Q};
+use blueprint::{Graph, Index, JoinField};
 use derive_setters::Setters;
-use valid::Validator;
 
 use crate::error::Error;
 use crate::Builder;
@@ -63,7 +63,7 @@ impl TypeName {
 pub struct SelectionSet<Value>(Vec<Field<Value>>);
 
 impl<Value: Default> SelectionSet<Value> {
-    pub fn new(doc: &ExecutableDocument, index: Rc<Index>) -> Self {
+    pub fn new(doc: &Q::ExecutableDocument, index: Rc<Index>) -> Self {
         let op = match &doc.operations {
             async_graphql_parser::types::DocumentOperations::Single(op) => &op.node,
             _ => todo!(),
@@ -132,4 +132,46 @@ pub enum Lens {
     Combine(Box<Lens>, Box<Lens>),
     ForEach(Box<Lens>),
     Empty,
+}
+
+impl<A: Default> From<&Q::SelectionSet> for SelectionSet<A> {
+    fn from(node: &Q::SelectionSet) -> SelectionSet<A> {
+        let mut selection_set: SelectionSet<A> = SelectionSet::default();
+        for selection in node.items.iter() {
+            let inner_selection = &selection.node;
+            match inner_selection {
+                Q::Selection::Field(Positioned { node, .. }) => {
+                    let field_name = node.name.node.as_str().to_string();
+                    let field =
+                        Field::new(field_name, SelectionSet::from(&node.selection_set.node));
+                    selection_set.push(field);
+                }
+                Q::Selection::InlineFragment(Positioned { node, .. }) => {
+                    todo!()
+                }
+                Q::Selection::FragmentSpread(Positioned { node, .. }) => {
+                    todo!()
+                }
+            }
+        }
+        selection_set
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{Builder, SelectionSet};
+    use insta::assert_debug_snapshot;
+
+    #[test]
+    fn test() {
+        let query = "query { topProducts { name reviews { score } reviews { description } } }";
+        let p_query = async_graphql_parser::parse_query(query).unwrap();
+        let op = match p_query.operations {
+            async_graphql_parser::types::DocumentOperations::Single(op) => op.node,
+            _ => todo!(),
+        };
+        let selection_set: SelectionSet<String> = Builder::new().build(&op);
+        assert_debug_snapshot!(selection_set);
+    }
 }

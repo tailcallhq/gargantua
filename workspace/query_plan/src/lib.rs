22c879e12394b3b88e3502adff27920503c4f5ef
Tushar Mathur <tusharmath@gmail.com>
2024-09-30 16:28:41 +0530

    add minify transformer

--- workspace/query_plan/src/lib.rs
+++ workspace/query_plan/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod error;
 mod model;
+mod transform;
 
 pub use model::*;
---
+++ workspace/query_plan/src/transform/minify.rs
@@ -0,0 +1,47 @@
+use std::marker::PhantomData;
+
+use valid::{Transform, Valid, Validator};
+
+use crate::error::Error;
+use crate::QueryPlan;
+pub struct Minify<A>(PhantomData<A>);
+
+impl<A> Transform for Minify<A> {
+    type Value = QueryPlan<A>;
+    type Error = Error;
+
+    fn transform(&self, input: Self::Value) -> Valid<Self::Value, Self::Error> {
+        match input {
+            QueryPlan::Parallel(items) => {
+                if items.len() == 1 {
+                    match items.into_iter().next() {
+                        Some(item) => self.transform(item),
+                        None => Valid::fail(Error::Plan(valid::Error::new(
+                            "Empty Parallel".to_string(),
+                        ))),
+                    }
+                } else {
+                    Valid::from_iter(items.into_iter(), |item| self.transform(item))
+                        .map(|items| QueryPlan::Parallel(items))
+                }
+            }
+            QueryPlan::Sequence(vec) => {
+                if vec.len() == 1 {
+                    match vec.into_iter().next() {
+                        Some(item) => self.transform(item),
+                        None => Valid::fail(Error::Plan(valid::Error::new(
+                            "Empty Sequence".to_string(),
+                        ))),
+                    }
+                } else {
+                    Valid::from_iter(vec.into_iter(), |item| self.transform(item))
+                        .map(|vec| QueryPlan::Sequence(vec))
+                }
+            }
+            QueryPlan::Fetch { .. } => Valid::succeed(input),
+            QueryPlan::Flatten { select, plan } => self
+                .transform(*plan)
+                .map(|plan| QueryPlan::Flatten { select, plan: Box::new(plan) }),
+        }
+    }
+}
